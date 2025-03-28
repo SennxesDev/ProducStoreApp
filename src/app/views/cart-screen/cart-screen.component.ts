a47d4661c750dd7cf09601dfe03752d1afba1701
Senn <100888352+SennxesDev@users.noreply.github.com>
2025-03-28 16:07:15 -0600

    Correccion de errores de carrito
    Se corrigieron errores relacionados al carrito y que este no añadia los productios

--- src/app/views/cart-screen/cart-screen.component.ts
+++ src/app/views/cart-screen/cart-screen.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CarritoService } from '../../services/services/carrito.service'; 
+import { Product } from '../../services/services/product.model'; 
 @Component({
   selector: 'app-cart-screen',
   templateUrl: './cart-screen.component.html',
   styleUrls: ['./cart-screen.component.scss']
 })
-export class CartScreenComponent {
-  cart = [
-    { name: 'Audífonos', image: 'assets/images/audifonos.jpg' },
-    { name: 'Smartwatch', image: 'assets/images/smartwatch.jpg' }
-  ];
+export class CartScreenComponent implements OnInit {
+  cart: Product[] = []; // Ahora especificamos que es un arreglo de productos
+
+  constructor(private carritoService: CarritoService) {}
+
+  ngOnInit() {
+    this.cart = this.carritoService.getCarrito(); // Obtiene los productos del carrito al iniciar el componente
+  }
 }
--- src/app/views/products-details-screen/products-details-screen.component.ts
+++ src/app/views/products-details-screen/products-details-screen.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CarritoService } from '../../services/services/carrito.service'
+import { Product } from '../../services/services/product.model';  
 
 @Component({
   selector: 'app-products-details-screen',
@@ -7,17 +9,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./products-details-screen.component.scss']
 })
 export class ProductsDetailsScreenComponent {
-  product = { 
+  product: Product = { 
     id: 1, 
     name: 'Audífonos', 
     price: 500, 
-    description: 'Audífonos con cancelación de ruido.' 
+    description: 'Audífonos con cancelación de ruido.',
+    image: 'assets/images/audifonos.jpg' // Asegúrate de incluir la propiedad de imagen
   };
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private carritoService: CarritoService) {}
 
-  // Método para agregar al carrito
+ 
   addToCart() {
+    this.carritoService.addToCart(this.product);
     alert('Producto agregado al carrito');
   }
 
